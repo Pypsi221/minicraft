3a13a58983e12a8f2c7c87daf89e042dccc4cf06
Pypsi221 <valeriiakatsalap@gmail.com>
2025-09-07 13:30:45 +0300

    add player

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 from direct.showbase.ShowBase import ShowBase
 from mapmanager import MapManager
-
+from heror import Hero
 class Game(ShowBase):
     def __init__ (self):
         super().__init__()
         base.camLens.setFov(90)
         land = MapManager()
-        x, y = land.load_map('maps/land3.txt')
-        
+        x, y = land.load_map('maps/land2.txt')
+        self.hero = Hero((x //2 ,y//2,1), land)
     
 game = Game()
 game.run()
